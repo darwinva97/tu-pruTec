474da0d39bc9c8f17bafd670d48c3d97d3f1dc4f
dsva <darwin97.va@gmail.com>
2023-06-06 21:47:30 -0500

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Layout } from "@/components/Layout";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Tu Prutec</title>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
